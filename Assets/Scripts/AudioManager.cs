8681290d2e45d50b341b1ca47ad0c454f5b80c50
DESKTOP-8S180T9\Freeeee <zxcvbn9779477@gmail.com>
2023-10-25 17:10:17 +0800

    初步音效功能製作

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+    public AudioSource soundSource;
+    public AudioClip soundEffect1; // 第一个音效
+    public AudioClip soundEffect2; // 第二个音效
+    private bool isSoundOn = true;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject); // 防止销毁
+            soundSource = GetComponent<AudioSource>();
+            soundSource.clip = soundEffect1;
+            soundSource.enabled = true;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlaySound1()
+    {
+        if (isSoundOn)
+        {
+            soundSource.clip = soundEffect1;
+            soundSource.Play();
+        }
+    }
+
+    public void PlaySound2()
+    {
+        if (isSoundOn)
+        {
+            soundSource.clip = soundEffect2;
+            soundSource.Play();
+        }
+    }
+
+    public void ToggleSound()
+    {
+        isSoundOn = !isSoundOn;
+        soundSource.enabled = isSoundOn;
+    }
+}
--- Assets/Scripts/LobbyGameManager.cs
+++ Assets/Scripts/LobbyGameManager.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -6,6 +6,15 @@ using UnityEngine.UI;
 
 public class LobbyGameManager : MonoBehaviour
 {
+
+    private AudioManager audioManager; // 用于存储对 AudioManager 的引用
+
+    private void Start()
+    {
+        // 在 Start 方法中查找 AudioManager 并引用它
+        audioManager = FindObjectOfType<AudioManager>();
+    }
+
     public void OnBtnLoadGameClick()
     {
         SceneManager.LoadScene("GameLoad");
@@ -25,5 +34,30 @@ public class LobbyGameManager : MonoBehaviour
     public void LoadSceneChooseMemory()
     {
         SceneManager.LoadScene("ChooseMemory");
+    }
+
+    // 调用 AudioManager 的 PlaySound 方法
+    public void PlaySound1()
+    {
+        if (audioManager != null)
+        {
+            audioManager.PlaySound1();
+        }
+        else
+        {
+            Debug.LogWarning("AudioManager not found.");
+        }
+    }
+
+    public void PlaySound2()
+    {
+        if (audioManager != null)
+        {
+            audioManager.PlaySound2();
+        }
+        else
+        {
+            Debug.LogWarning("AudioManager not found.");
+        }
     }
 }
